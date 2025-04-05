8e463dcbf1d1bbb48b411d36cd466c647ca3d4bb
komalkharche31 <komalkharche1989@gmail.com>
2025-04-05 14:38:45 +0530

    add todo list activity with array object

---
+++ use_array_state_todo_task/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import { Todolist } from './TodoList'
+function App() {  
+  return (
+    <>
+     <Todolist />
+    </>
+  )
+}
+
+export default App
---
+++ use_array_state_todo_task/src/TodoList.jsx
@@ -0,0 +1,70 @@
+import { useState } from "react"
+import { v4 as uuidv4 } from 'uuid';
+export function Todolist(){
+    let [Todos, setTodos] = useState([{task:"Simple task", key:uuidv4()}])
+    function AddNewTask() {
+        const getTask = document.querySelector("#TodoName");       
+        setTodos([
+            ...Todos,
+            {task:getTask.value, key:uuidv4()}
+        ])
+        document.querySelector('#TodoName').value = ''
+    }
+    function deleteList(key){
+        let copy = Todos.filter((obj)=>obj.key != key)
+        setTodos([...copy])
+    }
+
+    function doAllUperCase(){
+        let newTodoCopy = Todos.map((prevTodo)=>{
+               return {
+                ...prevTodo,
+                task:prevTodo.task.toUpperCase()
+               }
+        })
+        setTodos([...newTodoCopy])       
+    }
+
+    function doOneUperCase(key){
+        let newTodoCopy = Todos.map((prevTodo)=>{
+            if(prevTodo.key == key){
+                return {
+                    ...prevTodo,
+                    task:prevTodo.task.toUpperCase()
+                    }
+            }else{
+                return prevTodo
+            }
+               
+        })
+        setTodos([...newTodoCopy])
+    }
+
+    return (
+        <>
+            <input type="text" placeholder="Enter tasks" id="TodoName" style={{height:"35px"}}/>
+            <br />
+            <br />
+            <button onClick={AddNewTask}>Add</button>
+            <hr />           
+            <ul>
+            {
+                Todos.map((todo)=>(
+                    <li key={todo.key} style={{margin:"10px"}}>{todo.task}
+                    &nbsp;
+                    &nbsp;
+                    &nbsp;
+                    <button onClick={()=>deleteList(todo.key)}>Delete</button>
+                    &nbsp; &nbsp;
+                    <button onClick={()=>doOneUperCase(todo.key)}>UpperCase Me</button>
+                    </li> 
+                ))
+            }
+            <br />
+            <button onClick={doAllUperCase}>Create UpperCase</button>
+           
+
+            </ul> 
+        </>
+    )
+}
